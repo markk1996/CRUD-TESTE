9500893c9593ec3f47167437fe7c37c77cd853e5
markk1996 <47755145+markk1996@users.noreply.github.com>
2019-02-18 17:51:43 -0300

    Update deletar.php

--- deletar.php
+++ deletar.php
@@ -1,4 +1,5 @@
 <?php
+        //Deletar qualquer usuario na seleção listar atraves do id 
 	inclued_once(conexao.php);
 	$id= FILTER_INPUT(INPUT_GET,'ID',FILER_SANITIZE_NUMBER_INT);
 	$del= "delete  from usuarios where id= '$id'";
