c9a55b29dddd48ed8240f4a73aea55d1fb4a234a
markk1996 <47755145+markk1996@users.noreply.github.com>
2019-02-18 18:09:29 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -6,24 +6,21 @@
 		<title>CRUD - Listar</title>		
 	</head>
 	<body>
-		<a href="cad_usuario.php">Cadastrar</a><br>
-		<a href="index.php">Listar</a><br>
-		<h1>Listar Usuário</h1>
+		<a href="pros_cad.php">Cadastrar</a><br>
+		<h1>Listando Usuários</h1>
 		
 	<?php
+	// neste é realizado a listagem de todos os usuarios de acordo como foi salvo no banco de dados 
         include_once("conexao.php");
 		$list = "SELECT * FROM usuarios LIMIT $inicio, $qnt_result_pg";
 		$listar = mysqli_query($conn, $list);
+		//Criado um wille para listar todos os usuarios, com as opções de apagar ou editar
 		while($listar = mysqli_fetch_assoc($list)){
 			echo "ID: " . $listar['id'] . "<br>";
 			echo "Nome: " . $listar['nome'] . "<br>";
 			echo "E-mail: " . $listar['email'] . "<br>";
-			echo "Telefone-1: " . $listar['telefone_1'] . "<br>";
-			$tel = FILTER_INPUT(INPUT_GET,'telefone_2',FILER_SANITIZE_NUMBER_INT);
-			if(isset(tel)){
+			echo "Telefone-1: " . $listar['telefone_1'] . "<br>";			
 			echo "Telefone-2: " . $listar['telefone_2'] . "<br>";
-			}
-			unset(tel);
 			echo "<a href='editar.php?id=" . $listar['id'] . "'>Editar</a><br>";
 			echo "<a href='deletar.php?id=" . $listar['id'] . "'>Apagar</a><br><hr>";
 		}	
